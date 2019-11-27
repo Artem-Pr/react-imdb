c9f489bbf77921b00eedc8758188eb1155cf5eb5
Artem-Pr <pryadkin1987@gmail.com>
2019-11-27 14:45:59 +0200

    template

---
+++ components/Output/Output.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Output.module.css';
+
+function Output() {
+    return (
+        <section className={s.Output}>
+            Результаты поиска
+        </section>
+    );
+}
+
+export default Output;
