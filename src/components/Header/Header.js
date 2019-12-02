dd2f1f5ab2f0ebec798e1286df5aff5257be715a
Artem-Pr <pryadkin1987@gmail.com>
2019-12-02 23:07:20 +0200

    Added reactstrap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import s from './Header.module.css'
-import {NavLink} from "react-router-dom";
+import {NavLink} from 'react-router-dom';
+import {Button, Col, Container, Row} from 'reactstrap';
 
 function Header(props) {
     return (
         <header className={s.header}>
-            <img src={logo} className={s.logo} alt="logo"/>
-                <input type="text" className="form-control"
-                       value={props.searchText}
-                       onChange={(e) => props.onSearchChanged(e)}
-                       placeholder="Enter the name..."/>
-                <NavLink to={'/'}>
-                    <button onClick={() => props.onSearchClick()}>Search</button>
-                </NavLink>
+            <Container>
+                <Row>
+                    <Col xs="2" className="d-flex align-items-center">
+                        <img src={logo} className={s.logo} alt="logo"/>
+                    </Col>
+                    <Col xs="8" className="d-flex align-items-center">
+                        <input type="text" className="form-control"
+                               value={props.searchText}
+                               onChange={(e) => props.onSearchChanged(e)}
+                               placeholder="Enter the name..."/>
+                    </Col>
+                    <Col xs="2" className="d-flex align-items-center">
+                        <NavLink to={'/'}>
+                            <Button color='primary' onClick={() => props.onSearchClick()}>Search</Button>
+                        </NavLink>
+                    </Col>
+                </Row>
+            </Container>
         </header>
     );
 }
