151b9435174294d5e3d2f0722742c56d913c3bd0
Artem-Pr <pryadkin1987@gmail.com>
2020-01-06 12:25:09 +0200

    Added current month in request

--- src/api/api.js
+++ src/api/api.js
@@ -32,14 +32,21 @@ export const moviesAPI = {
 
     discoverMovies(lang, page) {
         console.log('Request - discover');
+        let today = new Date();
+        let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
+        let firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+        lastDayOfMonth = lastDayOfMonth.getFullYear() + '-' + lastDayOfMonth.getMonth()+1 + '-' + lastDayOfMonth.getDate();
+        firstDayOfMonth = firstDayOfMonth.getFullYear() + '-' + firstDayOfMonth.getMonth()+1 + '-01';
+        // console.log('firstDayOfMonth - ' + firstDayOfMonth);
+        // console.log('lastDayOfMonth - ' + lastDayOfMonth);
         return instance.get(`/discover/movie`, {
             params: {
                 api_key: tmdbAPI,
                 language: lang,
                 // sort_by: "popularity.desc",
                 // sort_by: "vote_average.desc",
-                "primary_release_date.gte": "2019-12-01",
-                "primary_release_date.lte": "2019-12-31",
+                "primary_release_date.gte": firstDayOfMonth,
+                "primary_release_date.lte": lastDayOfMonth,
                 // certification: "R",
                 include_adult: false,
                 include_video: false,
